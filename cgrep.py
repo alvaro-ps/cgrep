e662d67f5717ddcfa352552efc04860b8bf8ad80
apastors <alvaro@predicube.com>
2018-11-04 18:08:44 +0100

    list of regexes/colors

--- cgrep.py
+++ cgrep.py
@@ -4,26 +4,37 @@ from __future__ import print_function
 import sys 
 import re 
 import argparse 
+from itertools import chain
 
-from termcolor import colored
+from termcolor import colored, COLORS, HIGHLIGHTS
  
-def parseArguments(): 
-  parser = argparse.ArgumentParser('cgrep') 
-  parser.add_argument('regex', type=str) 
-  parser.add_argument('--color', type=str, default='red') 
-  return parser.parse_args(sys.argv[1:]) 
+ALL_COLORS = list(chain(COLORS, HIGHLIGHTS))
+
+def parse_arguments(): 
+    parser = argparse.ArgumentParser('cgrep') 
+    parser.add_argument(
+        'regexes',
+        nargs='+'
+    ) 
+    parser.add_argument(
+        '--colors',
+        nargs='+',
+        default=None
+    ) 
+    return parser.parse_args(sys.argv[1:]) 
   
 def replace_color(regex, string, color): 
-  str_list = regex.findall(string) 
-  for str_color in str_list: 
-    new_string = string.replace(str_color, colored(str_color, color))
-    string = new_string 
-  
-  return string 
+    str_list = regex.findall(string) 
+    for str_color in str_list: 
+        new_string = string.replace(str_color, colored(str_color, color))
+        string = new_string 
+      
+        return string 
 
 if __name__ == '__main__':
-  args = parseArguments()
-  regex = re.compile(args.regex)
-  color = args.color
-  for line in sys.stdin:
-    print(replace_color(regex, line, color), end='')
+    args = parse_arguments()
+    regexes = [re.compile(regex) for regex in args.regexes]
+    colors = args.colors
+    for line in sys.stdin:
+        for regex, color in zip(regexes, colors):
+            print(replace_color(regex, line, color), end='')
